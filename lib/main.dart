18d2687827bd938b34d3de8daa65624fcc2c5e6e
Keneuova Nagima <nagimakeneuova@gmail.com>
2024-01-26 00:39:35 +0600

    localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:chat_task/firebase_options.dart';
+import 'package:chat_task/generated/l10n.dart';
 import 'package:chat_task/pages/chatpage.dart';
 import 'package:chat_task/pages/home.dart';
 import 'package:chat_task/pages/signin.dart';
 import 'package:chat_task/pages/signup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,10 +21,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+          localizationsDelegates: [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
       title: 'Chat',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: SignUp(),
+      home: SignIn(),
     );
   }
 }
+
+
--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,3 +1,9 @@
+import 'package:chat_task/pages/home.dart';
+import 'package:chat_task/pages/signup.dart';
+import 'package:chat_task/service/database.dart';
+import 'package:chat_task/service/shared_pref.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -8,199 +14,251 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  String email = "", password = "", name = "", pic = "", username = "", id = "";
+  TextEditingController usermailcontroller = new TextEditingController();
+  TextEditingController userpasswordcontroller = new TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
+
+  userLogin() async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      QuerySnapshot querySnapshot =
+          await DatabaseMethods().getUserbyemail(email);
+
+      name = "${querySnapshot.docs[0]["Name"]}";
+      username = "${querySnapshot.docs[0]["username"]}";
+      pic = "${querySnapshot.docs[0]["Photo"]}";
+      id = querySnapshot.docs[0].id;
+
+      await SharedPreferenceHelper().saveUserDisplayName(name);
+      await SharedPreferenceHelper().saveUserName(username);
+      await SharedPreferenceHelper().saveUserId(id);
+      await SharedPreferenceHelper().saveUserPic(pic);
+
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Home()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.orangeAccent,
+            content: Text(
+              "No User Found for that Email",
+              style: TextStyle(fontSize: 18.0, color: Colors.black),
+            )));
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.orangeAccent,
+            content: Text(
+              "Wrong Password Provided by User",
+              style: TextStyle(fontSize: 18.0, color: Colors.black),
+            )));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           Container(
-            height: MediaQuery.of(context).size.height / 1.5,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color(0xFF1FDB5F),
-                  Color(0xFF31C764),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              borderRadius: BorderRadius.vertical(
-                bottom:
-                    Radius.elliptical(MediaQuery.of(context).size.width, 105.0),
-              ),
-            ),
-          ),
+              height: MediaQuery.of(context).size.height / 3.5,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                      bottom: Radius.elliptical(
+                          MediaQuery.of(context).size.width, 105.0)))),
           Padding(
             padding: const EdgeInsets.only(top: 70.0),
             child: Column(
               children: [
                 Center(
-                  child: Text(
-                    'Войдите',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
+                    child: Text(
+                  "SignIn",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold),
+                )),
                 Center(
-                  child: Text(
-                    'в свой аккаунт',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
+                    child: Text(
+                  "Login to your account",
+                  style: TextStyle(
+                      color: Colors.cyan,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500),
+                )),
                 SizedBox(
                   height: 20.0,
                 ),
                 Container(
-                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  margin:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
                   child: Material(
                     elevation: 5.0,
                     borderRadius: BorderRadius.circular(10),
                     child: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 50.0, horizontal: 20.0),
+                      height: MediaQuery.of(context).size.height / 2,
                       width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Почта",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(left: 10),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                width: 1,
-                                color: Colors.black38,
-                              ),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                prefixIcon: Icon(
-                                  Icons.mail_outline,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20.0,
-                          ),
-                          Text(
-                            "Пароль",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(left: 10),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                width: 1,
-                                color: Colors.black38,
-                              ),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                prefixIcon: Icon(
-                                  Icons.password,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              obscureText: true,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 16.0,
-                          ),
-                          Container(
-                            alignment: Alignment.bottomRight,
-                            child: Text(
-                              " Забыли пароль?",
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Form(
+                        key: _formkey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Почта",
                               style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.w500,
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 1.0, color: Colors.black38),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: TextFormField(
+                                controller: usermailcontroller,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Введите почту';
+                                  }
+                                  return null;
+                                },
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    prefixIcon: Icon(
+                                      Icons.mail_outline,
+                                      color: Colors.cyan,
+                                    )),
                               ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 40,
-                          ),
-                          Center(
-                            child: Material(
-                              elevation: 5,
-                              child: Container(
-                                width: 130,
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                    color: Colors.green,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Center(
-                                  child: Text(
-                                    'Вход',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Text(
+                              "Пароль",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 1.0, color: Colors.black38),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: TextFormField(
+                                controller: userpasswordcontroller,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Введите пароль';
+                                  }
+                                  return null;
+                                },
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    prefixIcon: Icon(
+                                      Icons.password,
+                                      color: Colors.cyan,
+                                    )),
+                                obscureText: true,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                              alignment: Alignment.bottomRight,
+                              child: Text(
+                                "Забыли пароль?",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 50.0,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                if (_formkey.currentState!.validate()) {
+                                  setState(() {
+                                    email = usermailcontroller.text;
+                                    password = userpasswordcontroller.text;
+                                  });
+                                }
+                                userLogin();
+                              },
+                              child: Center(
+                                child: SizedBox(
+                                  width: 130,
+                                  child: Material(
+                                    elevation: 5.0,
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Container(
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                          color: Colors.cyan,
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Center(
+                                          child: Text(
+                                        "Войти",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold),
+                                      )),
                                     ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
+                ),
+                SizedBox(
+                  height: 40.0,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      'Нет аккаунта?',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w500),
+                      "Не зарегистрированы?",
+                      style: TextStyle(color: Colors.black, fontSize: 16.0),
                     ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      'Зарегистрируйтесь',
-                      style: TextStyle(
-                          color: Color(0xFF1FDB5F),
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold),
-                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => SignUp()));
+                      },
+                      child: Text(
+                        " Sign Up Now!",
+                        style: TextStyle(
+                            color: Colors.cyan,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    )
                   ],
                 )
               ],
